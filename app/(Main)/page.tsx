0f52b29155bd2835ce8b3d540d8d7c5ee0d6bb60
DanialBabar <50171520+DanialBabar@users.noreply.github.com>
2024-03-10 12:53:46 +0500

    resolve main conflicts

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,17 +1,15 @@
 "use client";
-import DownloadAppBanner from "@/components/DownloadAppBanner";
+
 import Banner from "@/components/Layout/Banner";
 import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import PlatformCard from "@/components/PlatformCard";
 import RequestCard from "@/components/RequestCard";
-<<<<<<< HEAD
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
-=======
 import Image from "next/image";
->>>>>>> 6516295e3abba6d51d9b5ed5188e6014c9a46fe2
 import React from "react";
+import DownloadAppBanner from "@/components/DownloadAppBanner";
 
 type Props = {};
 
@@ -20,20 +18,22 @@ const page = (props: Props) => {
     <div dir="rtl" className="">
       <Header activeItem={0} />
       <Banner />
-<<<<<<< HEAD
-<div className="my-14 w-[90%] mx-auto flex justify-between items-center ">
-<h1 className="text-black text-4xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-[43px]  relative font-rbBold">طلبات        <span className="rounded-3xl bg-[#BDFFA5] pe-20 ps-2 h-6 py-0">
-التوريد</span>
-      </h1>
-      <div className="flex items-center">
-        <button className="bg-[#6B66DA] border-[2px] border-[#6B66DA] text-white rounded-full p-3"><MdKeyboardArrowRight className="text-4xl"/></button>
-      <button className="text-[#6B66DA] ms-3 border-[2px] border-[#6B66DA] rounded-full p-3"><MdOutlineKeyboardArrowLeft className="text-4xl"/></button>
+      <div className="my-14 w-[90%] mx-auto flex justify-between items-center ">
+        <h1 className="text-black text-4xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-[43px]  relative font-rbBold">
+          طلبات{" "}
+          <span className="rounded-3xl bg-[#BDFFA5] pe-20 ps-2 h-6 py-0">
+            التوريد
+          </span>
+        </h1>
+        <div className="flex items-center">
+          <button className="bg-[#6B66DA] border-[2px] border-[#6B66DA] text-white rounded-full p-3">
+            <MdKeyboardArrowRight className="text-4xl" />
+          </button>
+          <button className="text-[#6B66DA] ms-3 border-[2px] border-[#6B66DA] rounded-full p-3">
+            <MdOutlineKeyboardArrowLeft className="text-4xl" />
+          </button>
+        </div>
       </div>
-</div>
-
-      <RequestCard />
-      <Footer />
-=======
       <RequestCard />
       <div className="w-full flex flex-col items-center p-5 mt-10">
         <p className="text-black sm:text-[12px] text-[22px] md:text-4xl lg:text-6xl xl:text-[43px]  relative font-rbBold">
@@ -76,9 +76,8 @@ const page = (props: Props) => {
           </div>
         </div>
       </div>
-      <DownloadAppBanner/>
+      <DownloadAppBanner />
       <Footer />ٖ
->>>>>>> 6516295e3abba6d51d9b5ed5188e6014c9a46fe2
     </div>
   );
 };
